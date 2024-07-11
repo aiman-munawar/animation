aabd78b0a8c7bbd9326c08658b6199c2657f430a
aiman-munawar <aimanmunawar6@gmail.com>
2024-07-11 16:55:03 +0500

    add

---
+++ script.js
@@ -0,0 +1,43 @@
+var page=gsap.timeline();
+
+page.from("#nav h2",{
+     y:"-100vw",
+     duration:2,
+     opacity:0,
+     stagger:1,
+     
+})
+
+page.from(".image1 img",{
+    rotate:45,
+    duration:1,
+    opacity:0,
+    stagger:1
+
+})
+page.from("#image2",{
+    y:"-100vw",
+    duration:1
+
+})
+page.from("#image3",{
+    y:"-100vw",
+    duration:1,
+
+})
+page.from("#text h1",{
+    x:"-100vw",
+    duration:2,
+   opacity:"0.6vw",
+
+})
+page.from("#text h2",{
+    x:"-100vw",
+    duration:2,
+    opacity:"0.6vw",
+})
+page.from("#text h3",{
+    x:"-100vw",
+    duration:2,
+    opacity:"0.6vw",
+})
